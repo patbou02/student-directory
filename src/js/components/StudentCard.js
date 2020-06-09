a21d427a3fd7449b271f267554639b1dd5a07b29
Patrick Boutin <patrick.boutin@roberthalf.com>
2020-06-08 22:33:29 -0700

    Updated HTML of student card and adjusted CSS accordingly to make a more consistent look throughout all cards.

--- src/js/components/StudentCard.js
+++ src/js/components/StudentCard.js
@@ -2,14 +2,16 @@ const StudentCard = (obj, objKey) => {
 
   return `
     <div class="card directory-card">
-      <img class="card-img-top" src="${obj.avatar}" />
+      <div class="card-img-wrapper">
+        <img class="card-img-top" src="${obj.avatar}" />
+      </div>
       <div class="card-body">
         <h5 class="card-title">${obj.firstName} ${obj.lastName}</h5>
         <p class="card-text">${obj.email}<br />${obj.city}, ${obj.state}</p>
-        <div class="btn-container">
-          <a href="#/edit" type="button" id="${objKey}" class="btn btn-secondary">Edit</a>
-          <button type="button" id="${objKey}" class="btn btn-danger btn-delete">Delete</button>
-        </div>
+      </div>
+      <div class="card-footer">
+        <a href="#/edit" type="button" id="${objKey}" class="btn btn-secondary">Edit</a>
+        <button type="button" id="${objKey}" class="btn btn-danger btn-delete">Delete</button>
       </div>
     </div><!--/ .directory-card -->
   `;
